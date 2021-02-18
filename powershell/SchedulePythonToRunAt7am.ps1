81a86b62228e7e999b59c3a5ca1334bd8e2e7662
randolbj <77365403+randolbj@users.noreply.github.com>
2021-02-18 10:42:46 -0500

    Add files via upload

---
+++ powershell/SchedulePythonToRunAt7am.ps1
@@ -0,0 +1,5 @@
+﻿$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'C:\it30380c-scripts\Labs\webBrowserCode.py'
+
+$trigger = New-ScheduledTaskTrigger -Daily -At 10:25am 
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "PythonOpen2" -Description "brandon was here" 
